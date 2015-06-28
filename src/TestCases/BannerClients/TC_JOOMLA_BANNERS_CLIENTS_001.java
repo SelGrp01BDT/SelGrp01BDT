4ebd32e1d28c22efd28acb232712ab76eec0819c
BaTon <BaTon@BaTon-PC>
2015-06-28 17:33:43 +0700

    edit TC01

--- src/TestCases/BannerClients/TC_JOOMLA_BANNERS_CLIENTS_001.java
+++ src/TestCases/BannerClients/TC_JOOMLA_BANNERS_CLIENTS_001.java
@@ -1,16 +1,43 @@
 package TestCases.BannerClients;
 
-import org.openqa.selenium.WebDriver;
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
 import Common.AbstractTest;
 import Common.BrowserExecution;
+import Common.Config;
+import Pages.AdministratorPage;
+import Pages.BannerManagerClients;
+import Pages.LoginPage;
 
-public class TC_JOOMLA_BANNERS_CLIENTS_001 extends AbstractTest{
-
-	public WebDriver driver;
+public class TC_JOOMLA_BANNERS_CLIENTS_001 extends AbstractTest {
 	
-	
-	public TC_JOOMLA_BANNERS_CLIENTS_001(WebDriver driver) {
-		this.driver = driver;
+	@BeforeTest
+	public void setUp() throws InterruptedException {
+		driver = new FirefoxDriver();
+		BrowserExecution.navigateJoomla(driver);
 	}
-		
+
+	@AfterTest
+	public void tearDown() {
+		BrowserExecution.closeJoomla();
+	}
+
+	@Test
+	public void verify_create_new_client() throws Exception {
+
+		LoginPage login = new LoginPage(driver);
+		AdministratorPage adminpage = login.Do(Config.username_home,
+				Config.password_home);
+		BannerManagerClients bannermanager = adminpage.openBannerClients();
+		bannermanager.createNewClient("baton", "baton", "baton1211@gmail.com", "none", "saveandclose");
+
+		verifyTextPresent(driver, "Client successfully saved");
+
+	}
+
 }
