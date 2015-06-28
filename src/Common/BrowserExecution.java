2d5345c6c737e3024055e1136c7a2fc6e36e2594
BaTon <BaTon@BaTon-PC>
2015-06-28 17:34:07 +0700

    add open and exit Joomla method

--- src/Common/BrowserExecution.java
+++ src/Common/BrowserExecution.java
@@ -1,19 +1,31 @@
 package Common;
 
+import java.util.concurrent.TimeUnit;
+
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
+import Pages.LoginPage;
+
 public class BrowserExecution {
 
-	public void openSite(String url) {
-		driver.get(url);
+	public static String url_home = "http://localhost/JoomlaTest/administrator/index.php";
+	public static String url = "http://192.168.190.135/Joomla_2.5.28/administrator/index.php";
+	
+	protected static WebDriver driver;
+	protected String testUrl, browser;
+	
+	public static void navigateJoomla(WebDriver driver) {
+		driver.get(url_home);
+		driver.manage().timeouts().implicitlyWait(3, TimeUnit.SECONDS);
+		driver.manage().window().maximize();
 	}
-
-	public void closeSite()
+	
+	public static void closeJoomla()
 	{
 		driver.quit();
 	}
 	
-	private WebDriver driver = new FirefoxDriver();
+	
 
 }
